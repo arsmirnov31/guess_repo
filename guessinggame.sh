ff1dcb7dfedc60e7260d4241eb11e60c6cd8e46f
avsmirnov31@gmail.com <avsmirnov31@gmail.com>
2019-11-24 13:06:02 +0300

    Final version here

---
+++ guessinggame.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env bash
+# File: guessinggame.sh
+
+numbers_of_files=$(ls -1 -p | grep -v / | wc -l)
+
+function input {
+echo "Input your number"
+read user_number
+check_character
+}
+
+
+function game_begin {
+if [[ $user_number -lt $numbers_of_files ]]
+then 
+	echo "It is too small"
+	input
+elif [[ $user_number -gt $numbers_of_files ]]
+then
+	echo "It is to high"
+	input
+else 
+	echo "Current directory has $user_number files"
+fi
+}
+
+function check_character {
+if [[ $user_number =~ ^[[:digit:]]+$ ]] 
+then 
+	game_begin
+else
+ while [[ ! $user_number =~ ^[[:digit:]]+$ ]]
+ do
+	echo "Your input is not a number" 
+	read user_number
+ done
+ game_begin
+fi
+}
+
+
+check_character
+
